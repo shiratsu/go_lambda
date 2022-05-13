07248218b6630c8cd2bad4fffc497719ce597992
SHUNSUKE HIRATSUKA <shunsukehiratsuka@SHUNSUKEs-MacBook-Pro.local>
2022-05-13 13:28:21 +0900

    dynamo-dbの使い方途中

--- app/server.go
+++ app/server.go
@@ -6,9 +6,14 @@ import (
 
 func main() {
     http.HandleFunc("/", handler)
+    http.HandleFunc("/dynamo_test", handlerDynamoTest)
     http.ListenAndServe(":8080", nil)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Your url path is %s", r.URL.Path[1:])
 }
+
+
+func handlerDynamoTest(w http.ResponseWriter, r *http.Request) {
+}    
